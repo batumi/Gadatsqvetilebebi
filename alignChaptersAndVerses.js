88ef9a9010597b9a72b22278fff8413f7ef759c0
cesine <cesine@yahoo.com>
2014-05-12 13:50:21 +0400

    node.io is deprecated
    https://github.com/chriso/node.io
    updating to use request cheerio and async

--- alignChaptersAndVerses.js
+++ alignChaptersAndVerses.js
@@ -5,81 +5,92 @@
 //
 ////
 
+var extract = function($) {
 
-var book = document.body.getAttribute('id');
-var chapters = {};
 
-// check if a value is a number
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
+  var book = $('body').attr('id');
+  var chapters = {};
 
-// main function that starts with the entire document
-// and finds each language in the document
-$('body').find('.language').each(function() {
-
-  var languagecode = this.getAttribute('id');
-
-  // verify if all chapters for each language have the same number of verses
-  $(this).children('.chapter').each(function() {
-    try {
-     var chapter = $(this).children('.label')[0].innerHTML;
-   } catch (err) {
-    alert('This chapter did not download correctly.');
+  // check if a value is a number
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
-  // cycle through each chapter to find all of the verses.
-  // For each of these verses, save the number and the text for this verse
-  // and creates aligned verse groups.
-  (function(chapterNum, chapterDiv, langCode) {
-    chapterNumber = 'chapter' + chapterNum;
-    chapters[chapterNumber] = chapters[chapterNumber] || {};
-    chapters[chapterNumber]._chapterNumber = chapterNum;
-    chapters[chapterNumber].verses = chapters[chapterNumber].verses || {};
+  // main function that starts with the entire document
+  // and finds each language in the document
+  $('body').find('.language').each(function() {
 
-    $(chapterDiv).find('.verse').each(function() {
+    var languagecode = this.attr('id');
 
+    // verify if all chapters for each language have the same number of verses
+    $(this).children('.chapter').each(function() {
       try {
-        var verseNum = $(this).find('.label')[0].innerHTML;
-        verseNumber = 'verse' + verseNum;
-        console.log('working: ' , this);
-        chapters[chapterNumber].verses[verseNumber] = chapters[chapterNumber].verses[verseNumber] || {};
-        chapters[chapterNumber].verses[verseNumber]._verseNumber = verseNum;
-        chapters[chapterNumber].verses[verseNumber][langCode] = $(this).find('.content').html().replace(/\n/g,"").replace(/  */g," ");
+        var chapter = $(this).children('.label')[0].innerHTML;
       } catch (err) {
-        console.log('verse not working: ' , this);
+        alert('This chapter did not download correctly.');
       }
+
+      // cycle through each chapter to find all of the verses.
+      // For each of these verses, save the number and the text for this verse
+      // and creates aligned verse groups.
+      (function(chapterNum, chapterDiv, langCode) {
+        chapterNumber = 'chapter' + chapterNum;
+        chapters[chapterNumber] = chapters[chapterNumber] || {};
+        chapters[chapterNumber]._chapterNumber = chapterNum;
+        chapters[chapterNumber].verses = chapters[chapterNumber].verses || {};
+
+        $(chapterDiv).find('.verse').each(function() {
+
+          try {
+            var verseNum = $(this).find('.label')[0].innerHTML;
+            verseNumber = 'verse' + verseNum;
+            console.log('working: ', this);
+            chapters[chapterNumber].verses[verseNumber] = chapters[chapterNumber].verses[verseNumber] || {};
+            chapters[chapterNumber].verses[verseNumber]._verseNumber = verseNum;
+            chapters[chapterNumber].verses[verseNumber][langCode] = $(this).find('.content').html().replace(/\n/g, "").replace(/  */g, " ");
+          } catch (err) {
+            console.log('verse not working: ', this);
+          }
+        });
+      })(chapter, this, languagecode);
+
     });
-  })(chapter, this, languagecode);
 
-});
+  });
 
-});
-
-// Now that our "chapters" object is created and contains all of our aligned data, 
-// we will cycle through it to display the text information for the user in the browser
-var asRawText = '';
-for (chapter in chapters) {
-  for (verse in chapters[chapter].verses) {
-    var metadata = book + ':' + chapter.replace("chapter","") + ':' + chapters[chapter].verses[verse]._verseNumber;
-    for (language in chapters[chapter].verses[verse]) {
-      if (isNumber(chapters[chapter].verses[verse][language])) {
-        continue;
-      } else {
-        var thisline = metadata + ' ' + chapters[chapter].verses[verse][language] + '\n';
-        console.log(thisline);
-        asRawText += thisline;
+  // Now that our "chapters" object is created and contains all of our aligned data, 
+  // we will cycle through it to display the text information for the user in the browser
+  var asRawText = '';
+  for (chapter in chapters) {
+    for (verse in chapters[chapter].verses) {
+      var metadata = book + ':' + chapter.replace("chapter", "") + ':' + chapters[chapter].verses[verse]._verseNumber;
+      for (language in chapters[chapter].verses[verse]) {
+        if (isNumber(chapters[chapter].verses[verse][language])) {
+          continue;
+        } else {
+          var thisline = metadata + ' ' + chapters[chapter].verses[verse][language] + '\n';
+          console.log(thisline);
+          asRawText += thisline;
+        }
       }
+      asRawText += '\n';
     }
-    asRawText += '\n';
   }
+
+  // For useful data export, we will also convert our "chapters" object into XML and JSON.
+  var X2JS,
+    finalJSON,
+    xmlDocStr;
+
+  finalJSON = '{"book" : {"_book":"' + book + '", "chapters":' + JSON.stringify(chapters) + '}}';
+  if (X2JS) {
+    X2JS = new X2JS();
+    xmlDocStr = X2JS.json2xml_str(JSON.parse(finalJSON));
+  }
+  console.log(finalJSON);
+  $('body').prepend('<label class="json-label">JSON</label><textarea class="json">' + finalJSON + '</textarea>');
+  $('body').prepend('<label class="xml-label">XML</label><textarea class="xml"><?xml version="1.0" encoding="UTF-8"?><xml>' + xmlDocStr + '</xml></textarea>');
+  $('body').prepend('<label class="rawtext-label">Text</label><textarea class="rawtext">' + asRawText + '</textarea>');
 }
 
-// For useful data export, we will also convert our "chapters" object into XML and JSON.
-var X2JS = new X2JS();
-var finalJSON = '{"book" : {"_book":"' + book + '", "chapters":' + JSON.stringify(chapters) + '}}';
-var xmlDocStr = X2JS.json2xml_str(JSON.parse(finalJSON));
-
-$('body').prepend('<label class="json-label">JSON</label><textarea class="json">' + finalJSON + '</textarea>');
-$('body').prepend('<label class="xml-label">XML</label><textarea class="xml"><?xml version="1.0" encoding="UTF-8"?><xml>' + xmlDocStr + '</xml></textarea>');
-$('body').prepend('<label class="rawtext-label">Text</label><textarea class="rawtext">' + asRawText + '</textarea>');
+exports.extract = extract;
--- save.js
+++ save.js
@@ -1,14 +1,20 @@
-var nodeio = require('node.io');
-exports.job = new nodeio.Job({
-    input: false,
-    run: function () {
-        var url = this.options.args[0];
-        this.get(url, function(err, data) {
-            if (err) {
-                this.exit(err);
-            } else {
-                this.emit(data);
-            }
-        });
-    }
+var request = require('request')
+  , cheerio = require('cheerio')
+  , async = require('async')
+  , format = require('util').format
+  , siteSpecificExtractor = require("./genericExtractor").extract;
+
+var concurrency = 2;
+
+var urls = process.argv.splice(2);
+console.log(urls);
+async.eachLimit(urls, concurrency, function (url, next) {
+    console.log(url);
+    request(url, function (err, response, body) {
+        if (err) throw err;
+        var $ = cheerio.load(body);
+        var result = siteSpecificExtractor($);
+        console.log(result);
+        next();
+    });
 });
