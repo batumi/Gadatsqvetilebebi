9998f201e8632033156c34d250ecc18233687ff8
cesine <cesine@yahoo.com>
2014-05-12 15:46:06 +0400

    accepting numerous local or remote urls to extract html to utf8 text

--- save.js
+++ save.js
@@ -1,20 +1,48 @@
-var request = require('request')
-  , cheerio = require('cheerio')
-  , async = require('async')
-  , format = require('util').format
-  , siteSpecificExtractor = require("./genericExtractor").extract;
+#!/usr/local/bin/node
+
+var request = require('request'),
+  cheerio = require('cheerio'),
+  async = require('async'),
+  fs = require('fs'),
+  format = require('util').format,
+  siteSpecificExtractor = require('./constitutionalExtractor').extract;
 
 var concurrency = 2;
 
 var urls = process.argv.splice(2);
 console.log(urls);
-async.eachLimit(urls, concurrency, function (url, next) {
-    console.log(url);
-    request(url, function (err, response, body) {
-        if (err) throw err;
-        var $ = cheerio.load(body);
-        var result = siteSpecificExtractor($);
-        console.log(result);
-        next();
+
+
+var processHTMLFile = function(err, response, body, resultfilename, next) {
+  var result;
+  if (err) {
+    result = 'Not found';
+  } else {
+    // console.log(body);
+    var $ = cheerio.load(body);
+    result = siteSpecificExtractor($);
+  }
+  console.log(result);
+  fs.writeFile(resultfilename, result, 'utf8', function(err, data) {
+    if (err) {
+      console.log('Error writing file ' + resultfilename);
+    } else {
+      console.log('Wrote ' + resultfilename);
+    }
+  });
+  next();
+}
+
+async.eachLimit(urls, concurrency, function(url, next) {
+  var filename = url.substring(url.lastIndexOf('/') + 1);
+  console.log('Extracting text for ' + filename);
+  if (url.indexOf('://') > -1) {
+    request(url, function(err, response, body) {
+      processHTMLFile(err, response, body, filename.replace('.html', '.txt'), next)
     });
+  } else {
+    fs.readFile(url, 'utf8', function(err, data) {
+      processHTMLFile(err, null, data, filename.replace('.html', '.txt'), next);
+    });
+  }
 });
