a093401de095fe8270cc423747688fd39f0c3584
cesine <cesine@yahoo.com>
2014-05-13 11:50:07 +0400

    converting doc into html into txt for #3
    completed conversion of constitutional documents (to be verified)

--- save.js
+++ save.js
@@ -4,6 +4,7 @@ var request = require('request'),
   cheerio = require('cheerio'),
   async = require('async'),
   fs = require('fs'),
+  childProcess = require('child_process'),
   format = require('util').format,
   siteSpecificExtractor = require('./constitutionalExtractor').extract;
 
@@ -31,18 +32,75 @@ var processHTMLFile = function(err, response, body, resultfilename, next) {
     }
   });
   next();
-}
+};
+
+
+var processDOCFile = function(originalfilename, next) {
+  var result,
+    convertCommand = 'soffice --headless --convert-to html:html ',
+    path;
+
+  if (originalfilename.indexOf('/')) {
+    path = originalfilename.substring(0, originalfilename.lastIndexOf('/'));
+    convertCommand = convertCommand + ' --outdir ' + path + ' ';
+    // originalfilename.replace(path + '/', '');
+    console.log(convertCommand + originalfilename);
+  }
+  childProcess.exec(convertCommand + originalfilename, function(error, stdout, stderr) {
+    if (error !== null) {
+      console.log('Doc conversion error ' + originalfilename);
+      console.log(error);
+      console.log('Doc conversion stderr ' + stderr);
+      next();
+    } else {
+      console.log('Doc conversion stdout ' + stdout);
+
+      var htmlFilename = originalfilename.replace('.doc', '.html').replace('.docx', '.html');
+      console.log(htmlFilename);
+
+      fs.readFile(htmlFilename, 'utf8', function(err, data) {
+        processHTMLFile(err, null, data, htmlFilename.replace('.html', '.txt'), next);
+      });
+
+    }
+  });
+
+};
 
 async.eachLimit(urls, concurrency, function(url, next) {
   var filename = url.substring(url.lastIndexOf('/') + 1);
-  console.log('Extracting text for ' + filename);
+  var fileBaseName = filename.substring(0, filename.lastIndexOf('.'));
+  var extension = filename.replace(fileBaseName, '');
+
+  console.log('Extracting text for ' + fileBaseName);
+  console.log('  extension: ' + extension);
+
   if (url.indexOf('://') > -1) {
     request(url, function(err, response, body) {
-      processHTMLFile(err, response, body, filename.replace('.html', '.txt'), next)
+      if (filename.indexOf('.doc') === filename.length - 4 || filename.indexOf('.docx') === filename.length - 5) {
+        if (err) {
+          processHTMLFile(err, null, null, fileBaseName + '.txt', next);
+          return;
+        }
+        fs.writeFile(filename, body, 'utf8', function(err, data) {
+          if (err) {
+            console.log('Error writing file ' + filename);
+            next();
+            return;
+          }
+          processDOCFile(filename, next);
+        });
+      } else {
+        processHTMLFile(err, response, body, fileBaseName + '.txt', next);
+      }
     });
   } else {
-    fs.readFile(url, 'utf8', function(err, data) {
-      processHTMLFile(err, null, data, filename.replace('.html', '.txt'), next);
-    });
+    if (url.indexOf('.doc') === url.length - 4 || url.indexOf('.docx') === url.length - 5) {
+      processDOCFile(url, next);
+    } else {
+      fs.readFile(url, 'utf8', function(err, data) {
+        processHTMLFile(err, null, data, fileBaseName + '.txt', next);
+      });
+    }
   }
 });
