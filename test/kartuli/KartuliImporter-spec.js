954f1c14f6d56ceac016ee3c16888445f7fe4995
cesine <cesine@yahoo.com>
2014-06-05 23:19:42 +0400

    extending fielddb import

--- test/kartuli/KartuliImporter-spec.js
+++ test/kartuli/KartuliImporter-spec.js
@@ -1,7 +1,11 @@
 'use strict';
 
-var KartuliImporter = require('../../lib/kartuli/KartuliImporter.js').KartuliImporter;
+var KartuliImporter = require('./../../lib/kartuli/KartuliImporter.js').KartuliImporter;
+var Corpus = require('./../../node_modules/fielddb/api/corpus/Corpus').Corpus;
+
 var fs = require('fs');
+var specIsRunningTooLong = 5000;
+
 /*
   ======== A Handy Little Jasmine Reference ========
 https://github.com/pivotal/jasmine/wiki/Matchers
@@ -43,7 +47,6 @@ https://github.com/pivotal/jasmine/wiki/Matchers
 
 */
 
-var specIsRunningTooLong = 5000;
 
 describe('lib/KartuliImporter', function() {
   var corpus,
@@ -52,7 +55,7 @@ describe('lib/KartuliImporter', function() {
     remoteUri = 'https://raw.githubusercontent.com/OpenSourceFieldlinguistics/FieldDB/master/sample_data/orthography.txt';
 
   beforeEach(function() {
-    corpus = {};
+    corpus = new Corpus(Corpus.defaults);
     importer = new KartuliImporter({
       corpus: corpus
     });
@@ -62,8 +65,142 @@ describe('lib/KartuliImporter', function() {
     expect(KartuliImporter).toBeDefined();
   });
 
-  it('should provide a preprocess hook', function() {
-    expect(importer.preprocess).toBeDefined();
+  it("should be able to instantiate an object", function() {
+    var importer = new KartuliImporter();
+    expect(importer).toBeDefined();
   });
+
+  it("should be able to use a corpus", function() {
+    var dbname = "testingcorpusinimport-firstcorpus";
+    var corpus = new Corpus(Corpus.defaults);
+    expect(corpus).toBeDefined();
+    // console.log(corpus);
+    expect(corpus.dbname).toBeDefined();
+
+    var importer = new KartuliImporter({
+      corpus: corpus
+    });
+    expect(importer).toBeDefined();
+  });
+
+  it("should be able to ask the corpus to create a datum", function() {
+    var dbname = "testingcorpusinimport-firstcorpus";
+    var corpus = new Corpus(Corpus.defaults);
+    corpus.dbname = dbname;
+    var datum = corpus.newDatum();
+    console.log(datum);
+    expect(datum).toBeDefined();
+  });
+
+
+});
+
+describe("Batch KartuliImporter: as a morphologist I want to import directories of text files for machine learning", function() {
+  var corpus,
+    importer,
+    localUri = process.env.FIELDDB_HOME + '/FieldDB/sample_data/orthography.txt',
+    remoteUri = 'https://raw.githubusercontent.com/OpenSourceFieldlinguistics/FieldDB/master/sample_data/orthography.txt';
+
+  var defaultOptions = {
+    uri: localUri,
+    readOptions: {
+      readFileFunction: function(callback) {
+        fs.readFile(localUri, 'utf8', callback);
+      }
+    },
+    preprocessOptions: {
+      writePreprocessedFileFunction: function(filename, body, callback) {
+        fs.writeFile(filename, body, 'utf8', callback);
+      },
+      transliterate: true,
+      joinLines: true,
+    },
+    importOptions: {
+      dryRun: true,
+      fromPreprocessedFile: true
+    }
+  };
+
+  beforeEach(function() {
+    var dbname = "testingbatchimport-kartuli"
+    corpus = new Corpus(Corpus.defaults);
+    corpus.dbname = dbname;
+    corpus.language = {
+      "ethnologueUrl": "",
+      "wikipediaUrl": "",
+      "iso": "ka",
+      "locale": "",
+      "englishName": "",
+      "nativeName": "",
+      "alternateNames": ""
+    };
+    importer = new KartuliImporter({
+      dbname: dbname,
+      corpus: corpus
+    });
+    console.log('corpus', importer.corpus);
+  });
+
+
+  it('should provide a read file hook', function(done) {
+    expect(importer.preprocess).toBeDefined();
+    defaultOptions.rawText = "placeholder text ";
+
+    importer
+      .readUri({
+        uri: localUri,
+        readOptions: defaultOptions.readOptions
+      })
+      .then(function(result) {
+        console.log('after preprocess file');
+        expect(result.datum).toBeDefined();
+        expect(result.rawText.substring(0, 20)).toEqual('Noqata qan qaparinay');
+      })
+      .then(done, done);
+
+  }, specIsRunningTooLong);
+
+  it('should provide a preprocess hook', function(done) {
+    expect(importer.preprocess).toBeDefined();
+    // defaultOptions.rawText = "placeholder text ";
+    importer
+      .readUri(defaultOptions)
+      .then(importer.preprocess)
+      .then(function(result) {
+        console.log('after preprocess file');
+        expect(result.datum.datumFields.utterance).toBeDefined();
+        expect(result.preprocessedUrl).toEqual(process.env.FIELDDB_HOME + '/FieldDB/sample_data/orthography_preprocessed.json');
+
+        if (result.datum.datumFields.orthography.value !== result.rawText.trim()) {
+          expect(result.datum.datumFields.originalText.value)
+            .toEqual(result.rawText.trim());
+        } else {
+          expect(result.datum.datumFields.orthography.value)
+            .toEqual(result.rawText.trim());
+        }
+      })
+      .then(done, done);
+
+  }, specIsRunningTooLong);
+
+
+  it('should provide a addUri hook', function(done) {
+    expect(importer.preprocess).toBeDefined();
+    expect(importer.corpus).toBeDefined();
+    expect(importer.corpus.newDatum).toBeDefined();
+    defaultOptions.rawText = "placeholder text ";
+
+    importer
+      .addFileUri(defaultOptions)
+      .then(function(result) {
+        console.log('after preprocess file');
+        expect(result).toBeDefined();
+        // expect(result.datum.datumFields.utterance).toBeDefined();
+        // expect(result.preprocessedUrl).toEqual(process.env.FIELDDB_HOME + '/FieldDB/sample_data/orthography_preprocessed.json');
+
+      })
+      .then(done, done);
+
+  }, specIsRunningTooLong);
 
 });
